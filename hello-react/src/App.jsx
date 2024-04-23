e2dc09f1218b3011406eff22ecce466e069679a9
Dhruv <dhruvdm02@gmail.com>
2024-04-23 23:07:54 +0530

    changes

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let state ={
+    count: 0
+  }
+
+  function onClickHandeler(){
+    alert("hi")
+  }
 
   return (
-    <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+    <div>
+      <button onClick={onClickHandeler}>Counter {state.count}</button>
     </div>
   );
 }
 
-export default App
+export default App;
